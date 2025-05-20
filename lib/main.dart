57e44c020fcbe529fc52ebe288db2bbfde23d69a
Sevinc Abbasova <154439963+sevncabbsvaa@users.noreply.github.com>
2025-05-20 19:49:12 +0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
-main
+import 'pages/home_page.dart';
+
+void main() {
+  runApp(SpaceObjectsApp());
+}
+
+class SpaceObjectsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Space Objects Explorer',
-main
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        useMaterial3: true,
+      ),
+      home: HomePage(),
     );
   }
 }
